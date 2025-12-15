e5499cc1e9ed1cc24e41d9e63cb0340b1039afa6
GHOST100919 <ladejebijoshua@gmail.com>
2025-12-15 11:34:53 +0100

    letter

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 
-import Home from "../pages/home.jsx";
+import Home from "../pages/Home.jsx";
 import Visits from "../pages/Visits.jsx";
 import About from "../pages/About.jsx";
 import Contact from "../pages/Contact.jsx";
